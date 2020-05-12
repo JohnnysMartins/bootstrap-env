92bf45931c264cdccd938f7ce4a8664e748e6480
JohnnysMartins <johnnys.adson@gmail.com>
2020-05-12 19:25:23 -0300

    add telegram installer script

---
+++ scripts/telegram.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "<> Installing Telegram <>"
+
+sudo add-apt-repository ppa:atareao/telegram
+
+sudo apt install -y telegram
--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ chmod -R +x $PWD/scripts
 
 sh $PWD/scripts/init.sh
 sh $PWD/scripts/slack.sh
+sh $PWD/scripts/telegram.sh
 sh $PWD/scripts/insomnia.sh
 sh $PWD/scripts/spotify.sh
 sh $PWD/scripts/nvm.sh
