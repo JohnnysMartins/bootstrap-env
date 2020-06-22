16a9d3b6515526a6588dc0b671cb5dff6de22b8a
JohnnysMartins <johnnys.adson@gmail.com>
2020-06-22 13:57:07 -0300

    add docker script to run in start

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ echo "#----- START INSTALLATION -----#"
 chmod -R +x $PWD/scripts
 
 sh $PWD/scripts/init.sh
+sh $PWD/scripts/docker.sh
 sh $PWD/scripts/slack.sh
 sh $PWD/scripts/telegram.sh
 sh $PWD/scripts/insomnia.sh
